3b53689e7997a61d1a63e35bcc2e4a7ddd15b2a7
elizdem <91390634+elizdem@users.noreply.github.com>
2022-08-17 15:30:44 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.menu-icon-wrapper').onclick = function()
-{
-    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
-    document.querySelector('.nav__list-holder').classList.toggle('nav__list-holder--visible');
-}
